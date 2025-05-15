a4990ae6c9fb410af8ae5362f0932690c6f83bd5
alwil17 <willialfred24@gmail.com>
2025-05-15 16:58:31 +0000

    imported localizations

--- lib/screens/stats/my_stats_screen.dart
+++ lib/screens/stats/my_stats_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class MyStatsScreen extends StatelessWidget {
+class MyStatsScreen extends StatefulWidget {
   const MyStatsScreen({super.key});
 
   @override
+  State<MyStatsScreen> createState() => _MyStatsScreenState();
+}
+
+class _MyStatsScreenState extends State<MyStatsScreen> {
+
+  @override
   Widget build(BuildContext context) {
+    final locale = AppLocalizations.of(context)!;
     return const Placeholder();
   }
 }
