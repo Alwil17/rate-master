779b8c99af90df6faf81672d20d22c29a8ea5a0e
alwil17 <willialfred24@gmail.com>
2025-05-15 11:58:49 +0000

    Applyied delete action to item_details

--- lib/screens/items/item_detail_screen.dart
+++ lib/screens/items/item_detail_screen.dart
@@ -11,6 +11,8 @@ import 'package:rate_master/screens/items/widgets/item_detail_body.dart';
 import 'package:rate_master/screens/items/widgets/item_detail_header.dart';
 import 'package:rate_master/shared/theme/theme.dart';
 
+import 'dialogs/show_delete_review_dialog.dart';
+
 class ItemDetailScreen extends StatefulWidget {
   final num itemId;
 
@@ -168,44 +170,12 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
               padding: const EdgeInsets.symmetric(vertical: 16),
             ),
             onPressed: () async {
-              final locale = AppLocalizations.of(context)!;
-              // 1) show confirmation dialog
-              final confirm = await showDialog<bool>(
+              showDeleteReviewDialog(
                 context: context,
-                builder: (ctx) => AlertDialog(
-                  title: Text(locale.confirmDeleteTitle),
-                  content: Text(locale.confirmDeleteMessage),
-                  actions: [
-                    TextButton(
-                        onPressed: () => Navigator.of(ctx).pop(false),
-                        child: Text(locale.cancel)),
-                    TextButton(
-                        onPressed: () => Navigator.of(ctx).pop(true),
-                        child: Text(locale.delete)),
-                  ],
-                ),
+                ratingProvider: _ratingProvider,
+                itemId: item.id,
+                onSuccess: () => _ratingProvider.fetchMyReviews(_authProvider.user!.id),
               );
-
-              if (confirm == true) {
-                // 2) call provider
-                final success =
-                await _ratingProvider.deleteReviewForItem(item.id);
-                if (success) {
-                  // 3) refetch data and show feedback
-                  fetchItemDatas();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                        content: Text(
-                            locale.deleteSuccess)), // e.g. "Review deleted"
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                        content:
-                        Text(_ratingProvider.error ?? locale.deleteError)),
-                  );
-                }
-              }
             },
           ),
         ),
