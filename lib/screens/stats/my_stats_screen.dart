7142e52b455dd8d48f54c9ab8c1065ca4fd5cf03
alwil17 <willialfred24@gmail.com>
2025-05-15 17:00:53 +0000

    Added appbar to scaffold

--- lib/screens/stats/my_stats_screen.dart
+++ lib/screens/stats/my_stats_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class MyStatsScreen extends StatefulWidget {
   const MyStatsScreen({super.key});
@@ -13,6 +14,17 @@ class _MyStatsScreenState extends State<MyStatsScreen> {
   @override
   Widget build(BuildContext context) {
     final locale = AppLocalizations.of(context)!;
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mes avis"),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const PhosphorIcon(PhosphorIconsRegular.arrowLeft,
+              color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+    );
   }
 }
