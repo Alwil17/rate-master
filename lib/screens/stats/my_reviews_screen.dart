e673a9609fbc156ceea5692ba453f763a1a31f32
alwil17 <willialfred24@gmail.com>
2025-05-15 10:48:19 +0000

    Improved review tile

---
+++ lib/screens/stats/my_reviews_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:rate_master/models/rating.dart';
+import 'package:rate_master/shared/widgets/average_rating_display.dart';
+
+class MyReviewsScreen extends StatefulWidget {
+  const MyReviewsScreen({super.key});
+
+  @override
+  State<MyReviewsScreen> createState() => _MyReviewsScreenState();
+}
+
+class _MyReviewsScreenState extends State<MyReviewsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+  Widget _buildReviewTile(Rating review) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        title: Text(
+          "review.item.name",
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 4),
+            AverageRatingDisplay(
+              averageRating: review.value,
+              totalReviews: 0, // show only stars for this single review
+            ),
+            if (review.comment != null && review.comment!.isNotEmpty) ...[
+              const SizedBox(height: 6),
+              Text(
+                review.comment!,
+                style: const TextStyle(fontStyle: FontStyle.italic),
+              ),
+            ],
+            const SizedBox(height: 6),
+            Text(
+              DateFormat.yMMMd().format(review.createdAt!),
+              style: const TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+          ],
+        ),
+        trailing: PopupMenuButton<String>(
+          onSelected: (value) {
+            if (value == 'edit') {
+              // TODO: open edit form
+            } else if (value == 'delete') {
+              // TODO: confirm and delete
+            }
+          },
+          itemBuilder: (context) => const [
+            PopupMenuItem(value: 'edit', child: Text('Modifier')),
+            PopupMenuItem(value: 'delete', child: Text('Supprimer')),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
