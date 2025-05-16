3214e1fb198aa00b8497492f740b06e6047525a3
alwil17 <willialfred24@gmail.com>
2025-05-16 10:59:35 +0000

    Changed all static tile to custom element

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -3,6 +3,7 @@ import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:rate_master/providers/auth_provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:rate_master/shared/theme/theme.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -26,21 +27,14 @@ class SettingsScreen extends StatelessWidget {
           // Compte
           Text(locale.account, style: Theme.of(context).textTheme.titleLarge),
           const SizedBox(height: 8),
-          Card(
-            color: Colors.white,
-            borderOnForeground: true,
-            elevation: 0,
-            child: Column(
-              children: [
-                _buildSettingTile(
-                  user?.name ?? 'Utilisateur',
-                  user?.email ?? '',
-                  PhosphorIconsDuotone.user,
-                  null,
-                )
-              ],
-            ),
-          ),
+          _buildSettingTileContainer([
+            _buildSettingTile(
+              user?.name ?? 'Utilisateur',
+              user?.email ?? '',
+              PhosphorIconsDuotone.user,
+              null,
+            )
+          ]),
 
           const SizedBox(height: 24),
 
@@ -48,60 +42,61 @@ class SettingsScreen extends StatelessWidget {
           Text(locale.appearance,
               style: Theme.of(context).textTheme.titleLarge),
           const SizedBox(height: 8),
-          Card(
-            child: Column(
-              children: [
-                SwitchListTile(
-                  secondary: const Icon(Icons.brightness_6),
-                  title: Text(locale.darkMode),
-                  value: Theme.of(context).brightness == Brightness.dark,
-                  onChanged: (value) {
-                    // Implémenter changement de thème
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(Icons.language),
-                  title: Text(locale.language),
-                  trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-                  onTap: () {
-                    // Aller vers une page de sélection de langue
-                  },
-                ),
-              ],
-            ),
-          ),
+          _buildSettingTileContainer([
+            SwitchListTile(
+              secondary: const Icon(Icons.brightness_6),
+              title: Text(locale.darkMode),
+              value: Theme.of(context).brightness == Brightness.dark,
+              onChanged: (value) {
 
+              },
+            ),
+            _buildSettingTile(
+              locale.language,
+              null,
+              PhosphorIconsDuotone.globe, () {},
+            ),
+          ]),
           const SizedBox(height: 24),
 
           // À propos
           Text(locale.about, style: Theme.of(context).textTheme.titleLarge),
           const SizedBox(height: 8),
-          Card(
-            child: Column(
-              children: [
-                ListTile(
-                  leading: const Icon(Icons.info),
-                  title: Text(locale.appVersion),
-                  subtitle: const Text("1.0.0"),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.privacy_tip),
-                  title: Text(locale.privacyPolicy),
-                  onTap: () {
-                    // Ouvrir lien ou page
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(Icons.support_agent),
-                  title: Text(locale.contactSupport),
-                  onTap: () {
-                    // Lancer email ou page contact
-                  },
-                ),
-              ],
+          _buildSettingTileContainer([
+            _buildSettingTile(
+              locale.appVersion,
+              "1.0.0",
+              PhosphorIconsRegular.info,
+              null,
             ),
-          ),
+            _buildSettingTile(
+              locale.privacyPolicy,
+              null,
+              PhosphorIconsRegular.shieldWarning,
+              null,
+            ),
+            _buildSettingTile(
+              locale.contactSupport,
+              null,
+              PhosphorIconsRegular.headset,
+              null,
+            ),
+          ]),
         ],
+      ),
+    );
+  }
+
+  Widget _buildSettingTileContainer(List<Widget> childs) {
+    return Card(
+      color: Colors.white,
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+        side: BorderSide(color: AppColors.accent, width: 0.5),
+      ),
+      child: Column(
+        children: childs,
       ),
     );
   }
@@ -113,6 +108,9 @@ class SettingsScreen extends StatelessWidget {
       leading: PhosphorIcon(icon),
       title: Text(title),
       subtitle: subtitle != null ? Text(subtitle) : null,
+      trailing: onTap != null
+          ? PhosphorIcon(PhosphorIconsRegular.caretRight)
+          : null,
     );
   }
 }
