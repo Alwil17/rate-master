e8f75c7aa7c356e30b2df210bb0c51cfa3f61875
alwil17 <willialfred24@gmail.com>
2025-05-16 08:35:10 +0000

    Added content to activity page

--- lib/screens/stats/my_activity_screen.dart
+++ lib/screens/stats/my_activity_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
+import 'my_reviews_screen.dart';
+import 'my_stats_screen.dart';
+
 class MyActivityScreen extends StatelessWidget {
   const MyActivityScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 2, // Nombre d'onglets
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Mon activité'),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          bottom: const TabBar(
+            labelColor: Colors.black,
+            indicatorColor: Colors.blue,
+            tabs: [
+              Tab(text: 'Mes avis'),
+              Tab(text: 'Mes stats'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            MyReviewsScreen(),
+            MyStatsScreen(),
+          ],
+        ),
+      ),
+    );
   }
 }
