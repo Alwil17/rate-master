9dfe84461fe8044648d5a2de8d82f4c3688264da
alwil17 <willialfred24@gmail.com>
2025-05-16 08:36:40 +0000

    Changed router link for activity page

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -8,8 +8,7 @@ import 'package:rate_master/routes/routes.dart';
 import 'package:rate_master/screens/items/item_detail_screen.dart';
 import 'package:rate_master/screens/profile/profile_screen.dart';
 import 'package:rate_master/screens/search/search_screen.dart';
-import 'package:rate_master/screens/stats/my_reviews_screen.dart';
-import 'package:rate_master/screens/stats/my_stats_screen.dart';
+import 'package:rate_master/screens/stats/my_activity_screen.dart';
 import 'package:rate_master/shared/error_screen.dart';
 
 class AppRouter {
@@ -68,7 +67,7 @@ class AppRouter {
       GoRoute(
         path: APP_PAGES.stats.toPath,
         name: APP_PAGES.stats.toName,
-        builder: (context, state) => MyStatsScreen(),
+        builder: (context, state) => MyActivityScreen(),
       ),
       /*
       GoRoute(
