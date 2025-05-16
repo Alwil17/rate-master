c41914c10c3ff488e489b1cf43c63c4bbb022100
alwil17 <willialfred24@gmail.com>
2025-05-16 07:33:19 +0000

    Added CategoryPieChart struc

---
+++ lib/screens/stats/widgets/category_pie_chart.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryPieChart extends StatelessWidget {
+  const CategoryPieChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
