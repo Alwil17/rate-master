6132568dd5c3d2ec6732153f497d7b2ef3a3bdc8
alwil17 <willialfred24@gmail.com>
2025-05-19 16:59:09 +0000

    Refactored share app dialog widget

--- lib/screens/settings/widgets/share_app_dialog.dart
+++ lib/screens/settings/widgets/share_app_dialog.dart
@@ -16,10 +16,8 @@ class ShareAppDialog extends StatefulWidget {
 }
 
 class _ShareAppDialogState extends State<ShareAppDialog> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.transparent,
@@ -49,15 +47,27 @@ class _ShareAppDialogState extends State<ShareAppDialog> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          ClipRRect(borderRadius: BorderRadius.circular(30),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(30),
             child: Image.asset(
               Assets.imagesShootingStar,
               height: 175,
-            ),),
+            ),
+          ),
           const SizedBox(height: 15),
-          Text(Constants.appName, style: TextStyle(fontSize: 24, fontFamily: 'OpenSans', fontWeight: FontWeight.bold, color: Color(0xff056380)),),
+          Text(
+            Constants.appName,
+            style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                color: Color(0xff056380)),
+          ),
           const SizedBox(height: 10),
-          Text(AppLocalizations.of(context)!.share, style: TextStyle(fontSize: 18, fontFamily: 'OpenSans'),),
+          Text(
+            AppLocalizations.of(context)!.share,
+            style: TextStyle(fontSize: 18, fontFamily: 'OpenSans'),
+          ),
           const SizedBox(height: 20),
           SizedBox(
             height: 40,
@@ -68,34 +78,39 @@ class _ShareAppDialogState extends State<ShareAppDialog> {
                   onPressed: () => Navigator.of(context).pop(),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Color(0xff79a5b4),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
                   ),
                   icon: Icon(Icons.close, color: Color(0xff056380)),
                   label: Text(
                     AppLocalizations.of(context)!.cancel,
-                    style: const TextStyle(fontSize: 18, color: Color(0xff056380)),
+                    style:
+                        const TextStyle(fontSize: 18, color: Color(0xff056380)),
                   ),
                 ),
                 ElevatedButton.icon(
                   onPressed: () async {
                     final shareResult = await SharePlus.instance.share(
                       ShareParams(
-                        text: "${AppLocalizations.of(context)!.shareMessage} : https://play.google.com/store/apps/details?id=com.grey.rate_master",
+                        text:
+                            "${AppLocalizations.of(context)!.shareMessage} : https://play.google.com/store/apps/details?id=com.grey.rate_master",
                       ),
                     );
-                    if(shareResult.status == ShareResultStatus.success) {
+                    if (shareResult.status == ShareResultStatus.success) {
                       // Handle success
                       print("succeed");
                     }
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Color(0xffd9e8ee),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
                   ),
                   icon: Icon(Icons.share, color: Color(0xff056380)),
                   label: Text(
                     AppLocalizations.of(context)!.share,
-                    style: const TextStyle(fontSize: 18, color: Color(0xff056380)),
+                    style:
+                        const TextStyle(fontSize: 18, color: Color(0xff056380)),
                   ),
                 ),
               ],
