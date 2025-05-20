b1c8a30d6499ee514ef812bcf60883ff0c96f6e7
alwil17 <willialfred24@gmail.com>
2025-05-20 10:00:21 +0000

    Refactor: Improve About screen UI and link functionality

--- lib/screens/about/about_screen.dart
+++ lib/screens/about/about_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:rate_master/screens/about/widgets/bullet_button_item.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'widgets/bullet_item.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
-  void _openUrl(String url) async {
-    final uri = Uri.parse(url);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri, mode: LaunchMode.externalApplication);
+  Future<void> _launchUrl(String urlToOpen) async {
+    final Uri url = Uri.parse(urlToOpen);
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
     }
   }
 
@@ -18,67 +21,83 @@ class AboutScreen extends StatelessWidget {
     final locale = AppLocalizations.of(context)!;
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: PhosphorIcon(PhosphorIconsRegular.arrowLeft,
-              color: Theme.of(context).iconTheme.color),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        title: Text(locale.about)
-      ),
+          leading: IconButton(
+            icon: PhosphorIcon(PhosphorIconsRegular.arrowLeft,
+                color: Theme.of(context).iconTheme.color),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text(locale.about)),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
         child: ListView(
           children: [
             Text(
               "🎯 ${locale.aboutApp}",
-              style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                fontWeight: FontWeight.bold
-              ),
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 16),
+            Text(locale.appDescription,
+                style: Theme.of(context).textTheme.bodyMedium),
+            const SizedBox(height: 24),
             Text(
-            locale.appDescription,
-              style: Theme.of(context).textTheme.bodyMedium
+              '🛠️ ${locale.functionalities}',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            ListView(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                BulletItem(text: locale.addReviewFrom1To5),
+                BulletItem(text: locale.leaveAComment),
+                BulletItem(text: locale.viewStats),
+                BulletItem(text: locale.editPreferences),
+                BulletItem(text: locale.deleteAccount),
+              ],
             ),
             const SizedBox(height: 24),
-            const Text(
-              '🛠️ Fonctionnalités principales :',
-              style: TextStyle(fontWeight: FontWeight.bold),
+            Text(
+              '🔐 ${locale.datasPrivateAndSecure}',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium
+                  ?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              '📄 ${locale.learnMore}',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-            const Text('• Ajouter une note de 1 à 5'),
-            const Text('• Laisser un commentaire'),
-            const Text('• Consulter des statistiques d’évaluation'),
-            const Text('• Supprimer votre compte à tout moment'),
-            const SizedBox(height: 24),
-            const Text(
-              '🔐 Vos données sont privées et stockées en toute sécurité.',
-              style: TextStyle(fontStyle: FontStyle.italic),
-            ),
-            const SizedBox(height: 24),
-            const Text(
-              '📄 Pour en savoir plus :',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            ListTile(
-              contentPadding: EdgeInsets.zero,
-              title: const Text('• Politique de confidentialité'),
-              onTap: () => _openUrl(
+            BulletButtonItem(
+              text: locale.privacyPolicy,
+              onClick: () => _launchUrl(
                   'https://alwil17.github.io/rate-master/privacy-policy.html'),
             ),
-            ListTile(
-              contentPadding: EdgeInsets.zero,
-              title: const Text('• Conditions générales d’utilisation'),
-              onTap: () =>
-                  _openUrl('https://alwil17.github.io/rate-master/terms.html'),
+            BulletButtonItem(
+              text: locale.termsAndConditions,
+              onClick: () => _launchUrl(
+                  'https://alwil17.github.io/rate-master/terms-and-conditions.html'),
             ),
-            ListTile(
-              contentPadding: EdgeInsets.zero,
-              title: const Text('• Suppression de compte'),
-              onTap: () => _openUrl(
+            BulletButtonItem(
+              text: locale.childProtection,
+              onClick: () => _launchUrl(
                   'https://alwil17.github.io/rate-master/child-protection.html'),
             ),
+            BulletButtonItem(
+              text: locale.accountDeletion,
+              onClick: () => _launchUrl(
+                  'https://alwil17.github.io/rate-master/delete-account.html'),
+            )
           ],
         ),
       ),
